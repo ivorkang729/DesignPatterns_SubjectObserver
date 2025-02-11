b50d894357e7a32ceb82f17f73a007315bea9688
Oliver Kang <ivorkang729@gmail.com>
2025-02-11 23:51:48 +0800

    init

---
+++ src/main/java/ChannelSubscriber.java
@@ -0,0 +1,13 @@
+import java.nio.channels.Channel;
+
+public class ChannelSubscriber {
+    private String name;
+
+    public ChannelSubscriber(String name) {
+        this.name = name;
+    }
+
+    public void subscribe(Channel channel){
+
+    }
+}
---
+++ src/main/java/Video.java
@@ -0,0 +1,15 @@
+public class Video {
+    private String title;
+    private String description;
+    private Integer length;
+
+    public Video(String title, String description, Integer length) {
+        this.title = title;
+        this.description = description;
+        this.length = length;
+    }
+
+    public void like(ChannelSubscriber user){
+
+    }
+}
